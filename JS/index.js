7621e08c0c6c4d5a6b4d00828668806dc2b44b6a
ALYAkar <aliyafatmawati6@gmail.com>
2023-09-22 19:07:50 +0700

    Testimoni

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("testimonials");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(showDivs, 8000);  
+}
+
+
