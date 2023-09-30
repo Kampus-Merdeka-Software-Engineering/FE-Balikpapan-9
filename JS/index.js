67766656364ee460973f86ce1c8b08ed72502d1c
ALYAkar <aliyafatmawati6@gmail.com>
2023-09-30 17:27:56 +0700

    form

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 // Slide Home Alumni
 
-var slideIndex = 1;
+let slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -8,8 +8,8 @@ function plusDivs(n) {
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("testimonials");
+  let i;
+  let x = document.getElementsByClassName("testimonials");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
