d619851a6b9846916ce116bcdf06cb896d9782d7
HawwinEline <hawwineline12@gmail.com>
2023-10-01 19:35:19 +0700

    cek code

--- JS/index.js
+++ JS/index.js
@@ -4,21 +4,24 @@ let slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
   let i;
   let x = document.getElementsByClassName("testimonials");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
 
-  x[slideIndex-1].style.display = "block";
+  x[slideIndex - 1].style.display = "block";
 }
-
 
 // Hamburger Button
 
