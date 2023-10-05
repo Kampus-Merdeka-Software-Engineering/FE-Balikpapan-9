855c53232234400efdbf525f2b8a620a506a0330
ALYAkar <aliyafatmawati6@gmail.com>
2023-10-06 00:09:41 +0700

    cobaMateriBisa

--- JS/materi.js
+++
@@ -1,5 +0,0 @@
-fetch ('https://be-balikpapan-9-production.up.railway.app/materi/course/1')
-.then((response) => response.json())
-.then(data => {
-    console.log('data');
-})
---
+++ JS/materiFE.js
@@ -0,0 +1,24 @@
+const materiAjar = document.querySelector('.materialCourse');
+
+const GET = () => {
+    fetch ('https://be-balikpapan-9-production.up.railway.app/materi/course/3')
+    .then((response) => response.json())
+    .then(data => {
+        const list = data.data;
+        
+        for (let i = 0; i < list.length; i++) {
+            const ulMateri = document.createElement('ul');
+            ulMateri.setAttribute('class', 'listMateri');
+            materiAjar.appendChild(ulMateri);
+            const listMateri = document.getElementsByClassName('listMateri');
+            listMateri[i].innerHTML = `
+                <li>${list[i].materi}</li>
+            `
+        }
+    })
+.    catch(error => {
+        console.log(error);
+    })
+}
+
+GET()
