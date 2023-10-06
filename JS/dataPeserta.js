60210df6f007c3888d0202934ef66547a4d70cb7
Pras00 <prasetiawahyu22@gmail.com>
2023-10-06 10:13:27 +0700

    addPeserta

--- JS/dataPeserta.js
+++ JS/dataPeserta.js
@@ -25,8 +25,8 @@ const READ = () => {
             pesertaData[i].innerHTML = `
                 <td>${[i + 1]}</td>
                 <td>${Data[i].nama}</td>
-                <td>${Data[i].usia}</td>
                 <td>${Data[i].gender}</td>
+                <td>${Data[i].usia}</td>
                 <td>${Data[i].email}</td>
                 <td>${Data[i].no_telepon}</td>
                 <td>${Data[i].course.title}</td>
@@ -45,3 +45,44 @@ const READ = () => {
 };
 
 READ()
+
+
+
+// POST - CREATE Method
+const nama = document.getElementById('Name');
+const gender = document.getElementById('Gender');
+const age = document.getElementById('Age');
+const email = document.getElementById('Email');
+const no_telepon = document.getElementById('WhatsApp');
+const course = document.getElementById('Course');
+const submitForm = document.querySelector('.sendForm');
+
+submitForm.addEventListener('click', popupCloseForm);
+
+submitForm.addEventListener('click', () => {
+    const data = {
+        nama: nama.value,
+        gender: gender.value,
+        usia: parseInt(age.value),
+        email: email.value,
+        no_telepon: no_telepon.value,
+        title: course.value 
+    }
+
+    fetch('https://be-balikpapan-9-production.up.railway.app/peserta', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+        READ()
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+})
+
